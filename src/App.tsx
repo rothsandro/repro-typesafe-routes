301ff490d6d9dfdf7aabaf15fe9959c6d6dae06b
Sandro Roth <sandroroth@outlook.com>
2022-07-05 06:35:26 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, route } from "typesafe-routes";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+
+const homeRoute = route("/", {}, {});
+const aboutRoute = route("/about", {}, {});
 
 function App() {
+  console.log(`Route: "${homeRoute({}).$}"`);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <Routes>
+        <Route path={homeRoute.template} element={<Page />} />
+        <Route path={aboutRoute.template} element={<Page />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Link to={homeRoute({})}>Home</Link>
+      <Link to={aboutRoute({})}>About</Link>
     </div>
   );
 }
